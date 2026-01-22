3908931af11cc309fb4abcc06abf4ef02e867573
Dayasagar543 <dayasagar.543@gmail.com>
2026-01-23 01:01:17 +0530

    added the files in the templates according to the business requirements

---
+++ chinuch_tech/chinuch_tech/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request,"home.html")
+
+
+def aboutus(request):
+    return render(request,"AboutUs.html")
+
+def countactus(request):
+    return render(request,"ContactUs.html")
+
+def ourprograms(request):
+    return render(request,"Our_pragrams.html")
+
+def program_detailing_page(request):
+    return render(request,"Program_detailing_page.html")
+
+def page404(request):
+    return render(request,"Program_detailing_page.html")
