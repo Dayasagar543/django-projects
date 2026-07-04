d7b9b8263f335d4240cac568fbb2b339098ca34e
dayasagar543 <dayasagar.543@gmail.com>
2026-07-04 09:35:05 +0530

    added the urls and views

--- slate/slate/urls.py
+++ slate/slate/urls.py
@@ -20,5 +20,8 @@ from slate import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contactus, name='contactus'),
+    path('careers/', views.careers, name='careers'),
 ]
--- slate/slate/views.py
+++ slate/slate/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, World!")
+
+def about(request):
+    return HttpResponse("This is the about page.")
+
+def contactus(request):
+    return HttpResponse("This is the contact us page.")
+
+def careers(request):
+    return HttpResponse("This is the careers page.")
